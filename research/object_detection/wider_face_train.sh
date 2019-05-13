81070b705d3e8e9b676251fef76d5cdfa4f6593e
tamvm <tamvm@vng.com.vn>
2019-05-13 17:25:05 +0700

    minor

--- research/object_detection/wider_face_train.sh
+++ research/object_detection/wider_face_train.sh
@@ -1,6 +1,9 @@
 # From the tensorflow/models/research/ directory
-PIPELINE_CONFIG_PATH=./wider_face_models/ssd224/ssd_mobilenet_v2_quantized_224x224_widerface.config
-MODEL_DIR=./wider_face_models/ssd24
+# PIPELINE_CONFIG_PATH=./wider_face_models/ssd224/ssd_mobilenet_v2_quantized_224x224_widerface.config
+# MODEL_DIR=./wider_face_models/ssd224
+
+PIPELINE_CONFIG_PATH=./wider_face_models/ssd112/ssd_mobilenet_v2_quantized_112x112_widerface.config
+MODEL_DIR=./wider_face_models/ssd112
 NUM_TRAIN_STEPS=50000
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
 python model_main.py \
